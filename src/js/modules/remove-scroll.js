801aed96a0c3ff6d6e5048bb6bece9f0713949a8
glubokij1985 <glubokij85@gmail.com>
2018-12-18 23:01:17 +0200

    add video popup

--- src/js/modules/remove-scroll.js
+++ src/js/modules/remove-scroll.js
@@ -10,34 +10,34 @@ var removeScroll = {
             if (true) {
                 if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
                     var $fullSectionWrap = $('.full-section');
-                    $secondSectionWrap = $('.second-section');
-                    $body = $('body');
+                    var $secondSectionWrap = $('.second-section');
+                    var $body = $('body');
                     if (!$body.hasClass("hide-section")) {
                         $body.addClass('hide-section');
                         $fullSectionWrap.fadeOut(1000);
-                        $body.css({'overflow-y':'scroll'});
+                        $body.css({ 'overflow-y': 'scroll' });
+                        $body.scrollTop(0);
                     }
-                    
-                } 
+                }
             }
             return true;
         });
-        $(document).bind('touchstart', function (e){
-           ts = e.originalEvent.touches[0].clientY;
+        $(document).bind('touchstart', function (e) {
+            ts = e.originalEvent.touches[0].clientY;
         });
-        
-        $(document).bind('touchend', function (e){
-           var te = e.originalEvent.changedTouches[0].clientY;
-           if(ts > te+10){
-              var $fullSectionWrap = $('.full-section');
-                    $secondSectionWrap = $('.second-section');
-                    $body = $('body');
-                    if (!$body.hasClass("hide-section")) {
-                        $body.addClass('hide-section');
-                        $fullSectionWrap.fadeOut(1000);
-                        $body.css({'overflow-y':'scroll'});
-                    }
-           }
+
+        $(document).bind('touchend', function (e) {
+            var te = e.originalEvent.changedTouches[0].clientY;
+            if (ts > te + 10) {
+                var $fullSectionWrap = $('.full-section');
+                $secondSectionWrap = $('.second-section');
+                $body = $('body');
+                if (!$body.hasClass("hide-section")) {
+                    $body.addClass('hide-section');
+                    $fullSectionWrap.fadeOut(1000);
+                    $body.css({ 'overflow-y': 'scroll' });
+                }
+            }
         });
     }
 
--- src/js/modules/scroll-pages.js
+++ src/js/modules/scroll-pages.js
@@ -11,31 +11,36 @@ var scrollPages = {
         }
 
         var lastScrollTop = 0;
-        $(window).scroll(function(event){
+        $(window).scroll(function (event) {
             var st = $(this).scrollTop(),
                 $secondSectionWrapTop = $secondSectionWrap.offset().top;
-            if (st > lastScrollTop){
+            if (st > lastScrollTop) {
                 // downscroll code
-                if(st > 0){
+                if (st > 0) {
                     $body.addClass('hide-section');
                     $fullSectionWrap.fadeOut(1000);
                 }
             } else {
                 // upscroll code
-                if(st <= $secondSectionWrapTop){
+                if (st <= $secondSectionWrapTop) {
                     $body.removeClass('hide-section');
+                    $body.css({overflow: "hidden"});
                     $fullSectionWrap.fadeIn(1000);
                 }
             }
             lastScrollTop = st;
         });
 
-        $scrollUpBtn.on('click', function(){
+        $scrollUpBtn.on('click', function () {
+            var $fullSectionWrap = $('.full-section');
             $body.removeClass('hide-section');
+            $fullSectionWrap.fadeIn(1000);
         });
 
-        $scrollDownBtn.on('click', function(){
+        $scrollDownBtn.on('click', function () {
+            var $fullSectionWrap = $('.full-section');
             $body.addClass('hide-section');
+            $fullSectionWrap.fadeOut(1000);
         });
     }
 
